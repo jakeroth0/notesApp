e5c3099fe3dbcfb45110941c566e6fac5ac2c746
Jacob Roth <jakeroth0@gmail.com>
2023-01-30 10:43:09 -0500

    added notes to explain code and started working on the post routes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,17 +1,65 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-
+// fs is file server and allows node.js
+// to work with the file system on your computer
+const fs = require('fs')
+// sets db.json to a variable
+let db = require('../db/db.json')
+// all routes within this file happen at
+// the endpoint following /api (See the server.js)
+// We're getting the db.json and responding with it
 router.get('/notes', (req, res) => {
-    // res.send('figuring this out')
-    res.sendFile(path.join(__dirname,'/public/notes.html'))
-    console.log('notes')
-    console.log(__dirname,'/public/notes.html')
+    console.log(`${req.method} request recieved`)
+    console.log('get notes')
+    console.log(db)
+    // res is an var tied to an object in express
+    // this object has a function (which would be considered
+    // a method) "json" that handles json files
+    res.status(200).json(db)
 })
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-    console.log((__dirname, '../public/index.html'));
-  });
+
+router.post('/notes', (req, res) => {
+    console.log(`${req.method} request recieved`)
+
+    const { title, text } = req.body;
+
+    if (title && text ) {
+        const newNote = {
+            title,
+            text,
+        };
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNote = JSON.parse(data);
+                parsedNote.push(newNote);
+                
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNote, null, 2),
+                    (writeErr) =>
+                    writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully updated notes!')
+                )
+            }
+        })
+
+        const response = {
+          status: 'success',
+          body: newNote,  
+        }
+
+        console.log(response);
+        res.status(201).json(db);
+        console.log("getting db")
+        console.log(db);
+    } else {
+        res.status(500).json('Error in posting note');
+    }
+ });
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,13 +1,32 @@
+// this gives us access to express
 const express = require('express')
+// assigns notesRouter to the router middleware
+// so that we have access to the routes within our html script
+// notes.js
 const notesRouter = require('./routes/notes')
+// similar to the notes router, this gives us access
+// to api routes through the router middleware
+const apiRouter = require('./routes/apiroutes')
 
+// assigns the express function as an ap
 const app = express()
+// server host OR port 3000
 const PORT = process.env.PORT || 3000;
 
-
+// middleware function that parses incoming JSON req
+// and puts parsed data in req.body
 app.use(express.json());
+// above and below are needed for POST and PUT req
+// below recognizes incoming req objects as strings or arrays
+// below can also handle html post form
 app.use(express.urlencoded({ extended: true }))
+// makes it possible to access static files
+// via html
 app.use(express.static('public'))
+// for any req with /api use apiRouter
+app.use('/api', apiRouter)
+// for any req with / use notesRouter
 app.use('/', notesRouter)
-
+// tells user that listening for actions on whatever
+// PORT is set to
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
